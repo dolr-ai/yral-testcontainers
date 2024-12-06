e2110e0f4b5fca893c181cb2ed7e4868b0838b55
tushar-saxena-yral <tushar@gobazzinga.io>
2024-12-06 07:52:43 +0530

    feat: expose 50051 port for off chain container

--- src/off_chain_agent.rs
+++ src/off_chain_agent.rs
@@ -7,9 +7,11 @@ pub struct OffChainAgent {
     tag: String,
 }
 
+pub const OFF_CHAIN_AGENT_PORT: ContainerPort = ContainerPort::Tcp(50051);
+
 impl OffChainAgent {
     pub fn new(tag: &str) -> Self {
-        Sefl { tag: tag.into() }
+        Self { tag: tag.into() }
     }
 }
 
@@ -24,5 +26,9 @@ impl Image for OffChainAgent {
 
     fn ready_conditions(&self) -> Vec<WaitFor> {
         vec![WaitFor::millis(1000)]
+    }
+
+    fn expose_ports(&self) -> &[ContainerPort] {
+        &[OFF_CHAIN_AGENT_PORT]
     }
 }
